fd19ee6c162a89848fb2d861cec630b93095252d
maykmoreira <mayk.hsm@gmail.com>
2020-04-21 16:02:22 -0300

    Animation HomePage

--- lib/pages/home/widgets/page_view_app.dart
+++ lib/pages/home/widgets/page_view_app.dart
@@ -4,25 +4,33 @@ import 'package:flutter/material.dart';
 class PageViewApp extends StatelessWidget {
   final double top;
   final ValueChanged<int> onChanged;
-  const PageViewApp({Key key, this.top, this.onChanged}) : super(key: key);
+  final GestureDragUpdateCallback onPanUpdate;
+  final bool showMenu;
 
+  const PageViewApp({Key key, this.top, this.onChanged, this.onPanUpdate, this.showMenu})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
+    return AnimatedPositioned(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
         top: top,
         height: MediaQuery.of(context).size.height * .45,
         left: 0,
         right: 0,
-        child: SizedBox(
-          child: PageView(
-            onPageChanged: onChanged,
-            physics: BouncingScrollPhysics(),
-            children: <Widget>[
-              CardApp(),
-              CardApp(),
-              CardApp(),
-            ],
+        child: GestureDetector(
+          onPanUpdate: onPanUpdate,
+          child: SizedBox(
+            child: PageView(
+              onPageChanged: onChanged,
+              physics: showMenu ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(),
+              children: <Widget>[
+                CardApp(),
+                CardApp(),
+                CardApp(),
+              ],
+            ),
           ),
         ));
   }
